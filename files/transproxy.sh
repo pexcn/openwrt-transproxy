5e3a24040ffb11a701e5645cb703b1e211bfe1a6
pexcn <i@pexcn.me>
2021-11-04 10:25:53 +0800

    update

--- files/transproxy.sh
+++ files/transproxy.sh
@@ -6,48 +6,112 @@
 # See /LICENSE for more information.
 #
 
-log() {
-  # 1:alert, 2:crit, 3:err, 4:warn, 5:notice, 6:info, 7:debug, 8:emerg
-  logger -st tp-rules[$$] -p $1 $2
+_get_reserved_ipv4() {
+  cat <<- EOF
+	0.0.0.0/8
+	10.0.0.0/8
+	100.64.0.0/10
+	127.0.0.0/8
+	169.254.0.0/16
+	172.16.0.0/12
+	192.0.0.0/24
+	192.0.2.0/24
+	192.31.196.0/24
+	192.52.193.0/24
+	192.88.99.0/24
+	192.168.0.0/16
+	192.175.48.0/24
+	198.18.0.0/15
+	198.51.100.0/24
+	203.0.113.0/24
+	224.0.0.0/3
+	EOF
 }
 
-gen_ignore_ip_list() {
-0.0.0.0/8
-10.0.0.0/8
-100.64.0.0/10
-127.0.0.0/8
-169.254.0.0/16
-172.16.0.0/12
-192.0.0.0/24
-192.0.2.0/24
-192.31.196.0/24
-192.52.193.0/24
-192.88.99.0/24
-192.168.0.0/16
-192.175.48.0/24
-198.18.0.0/15
-198.51.100.0/24
-203.0.113.0/24
-224.0.0.0/3
+_get_reserved_ipv6() {
+  # TODO: redo
+  cat <<- EOF
+	::/127
+	::ffff:0:0/96
+	::ffff:0:0:0/96
+	64:ff9b::/96
+	100::/64
+	2001::/32
+	2001:20::/28
+	2001:db8::/32
+	2002::/16
+	fc00::/7
+	fe80::/10
+	ff00::/8
+	EOF
+}
 
-::/127
-::ffff:0:0/96
-::ffff:0:0:0/96
-64:ff9b::/96
-100::/64
-2001::/32
-2001:20::/28
-2001:db8::/32
-2002::/16
-fc00::/7
-fe80::/10
-ff00::/8
+_add_prefix() {
+  sed "s/^/$1/"
+}
+
+_remove_empty() {
+  sed '/^[[:space:]]*$/d'
+}
+
+_remove_comment() {
+  sed '/^#/ d'
+}
+
+_ipset_execute() {
+  ipset -exist restore <<- EOF
+	$@
+	EOF
+}
+
+PREFIX_CHAIN="TRANSPROXY_"
+
+log() {
+  # 1:alert, 2:crit, 3:err, 4:warn, 5:notice, 6:info, 7:debug, 8:emerg
+  logger -st transproxy[$$] -p $1 $2
+}
+
+print_usage() {
+
 }
 
 flush_rules() {
+  # iptables
+  iptables-save --counters | grep -v "$PREFIX_CHAIN" | iptables-restore --counters
+
+  # route
+  ip rule del fwmark 1 lookup 100 2>/dev/null || true
+  ip route del local default dev lo table 100 2>/dev/null || true
+  ip route flush table 100 2>/dev/null || true
+
+  # ipset
+  for name in $(ipset -n list | grep "$PREFIX_CHAIN"); do
+    ipset flush $name 2>/dev/null || true
+    ipset destroy $name 2>/dev/null || true
+  done
 }
 
-ipset_init() {
+init_ipset() {
+  # create
+  _ipset_execute create transproxy_src_direct hash:ip hashsize 64 family inet
+  _ipset_execute create transproxy_src_global hash:ip hashsize 64 family inet
+  _ipset_execute create transproxy_src_normal hash:ip hashsize 64 family inet
+  _ipset_execute create transproxy_dst_direct hash:net hashsize 64 family inet
+  _ipset_execute create transproxy_dst_global hash:net hashsize 64 family inet
+  _ipset_execute create transproxy_dst_normal hash:net hashsize 64 family inet
+
+  # reserved
+  _ipset_execute "$(_get_reserved_ipv4 | _add_prefix 'transproxy_dst_direct ')"
+
+  # src sets
+  _ipset_execute "$(cat /etc/transproxy/src-direct.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_src_direct ')"
+  _ipset_execute "$(cat /etc/transproxy/src-global.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_src_global ')"
+  _ipset_execute "$(cat /etc/transproxy/src-normal.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_src_normal ')"
+
+  # dst sets
+  _ipset_execute "$(cat /etc/transproxy/dst-direct.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_dst_direct ')"
+  _ipset_execute "$(cat /etc/transproxy/dst-global.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_dst_global ')"
+  _ipset_execute "$(cat /etc/transproxy/dst-normal.txt | _remove_empty | _remove_comment | _add_prefix 'transproxy_dst_normal ')"
 }
 
 iptables_nat() {
@@ -57,80 +121,39 @@ iptables_mangle() {
 }
 
 parse_args() {
-  while getopts ":r" arg; do
-    case "$arg" in
-      s)
-        server=
+  while [ "$#" -gt 0 ]; do
+    case "$1" in
+      -r|--remote)
+        REMOTE_ADDR="$2"
+        shift 2
+        ;;
+      -p|--port)
+        REMOTE_PORT="$2"
+        shift 2
+        ;;
+      --dst-forward-recentrst)
+        DST=1
+        shift 1
+        ;;
+      -h|--help)
+        print_usage
+        exit 0
+        ;;
+      *)
+        echo "unknown option $1"
+        return 1
+        ;;
+    esac
   done
 }
 
-while getopts ":s:l:S:L:B:b:W:w:I:d:a:e:oOuUfh" arg; do
-	case "$arg" in
-		s)
-			server=$(for ip in $OPTARG; do echo $ip; done)
-			;;
-		l)
-			local_port=$OPTARG
-			;;
-		S)
-			SERVER=$(for ip in $OPTARG; do echo $ip; done)
-			;;
-		L)
-			LOCAL_PORT=$OPTARG
-			;;
-		B)
-			WAN_BP_LIST=$OPTARG
-			;;
-		b)
-			WAN_BP_IP=$OPTARG
-			;;
-		W)
-			WAN_FW_LIST=$OPTARG
-			;;
-		w)
-			WAN_FW_IP=$OPTARG
-			;;
-		I)
-			IFNAMES=$OPTARG
-			;;
-		d)
-			LAN_TARGET=$OPTARG
-			;;
-		a)
-			LAN_HOSTS=$OPTARG
-			;;
-		e)
-			EXT_ARGS=$OPTARG
-			;;
-		o)
-			OUTPUT=SS_SPEC_WAN_AC
-			;;
-		O)
-			OUTPUT=SS_SPEC_WAN_FW
-			;;
-		u)
-			TPROXY=1
-			;;
-		U)
-			TPROXY=2
-			;;
-		f)
-			flush_rules
-			exit 0
-			;;
-		h)
-			usage 0
-			;;
-	esac
-done
-
-
 main() {
-  parse_args
 	flush_rules && ipset_init && iptables_nat && iptables_mangle
 	RET=$?
 	[ "$RET" = 0 ] ||  log 3 "Start failed!"
 	exit $RET
 }
 
-main
+parse_args "$@"
+flush_rules
+init_ipset
